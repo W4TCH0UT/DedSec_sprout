e11d0399d452367e32d85ba42e21e40e3bc68941
Ateekujjawal <ateekujjawal@gmail.com>
2014-11-10 19:51:51 +0530

    Compile with linaro 4.9.2

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-export CROSS_COMPILE="/root/linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-"
+export CROSS_COMPILE="~/linaro-4.9/bin/arm-linux-gnueabihf-"
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER="w4tchout"
 export KBUILD_BUILD_HOST="WarMachine"
-make sprout_defconfig
-make
+make -j5 -o3
