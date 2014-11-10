4e0dfeb870811cdab87f86d1982add84313d9c58
Ateekujjawal <ateekujjawal@gmail.com>
2014-11-10 17:33:18 +0530

    Add o3 optimizations

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 export CROSS_COMPILE="/root/linaro/4.7.3-2013.04.20130415/bin/arm-linux-gnueabihf-"
 export ARCH=arm
 export SUBARCH=arm
-export KBUILD_BUILD_USER="varun.chitre15"
-export KBUILD_BUILD_HOST="Monster-Machine"
+export KBUILD_BUILD_USER="w4tchout"
+export KBUILD_BUILD_HOST="WarMachine"
 make sprout_defconfig
 make
